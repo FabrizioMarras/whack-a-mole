8006818dedd4320d66b1200a249636eea3262d0a
Fabrizio Marras <fmarras79@gmail.com>
2018-10-14 21:44:30 +0200

    added easy/difficult level selection.

--- js/game.js
+++ js/game.js
@@ -3,10 +3,14 @@ const holes = document.querySelectorAll('.hole'); //select all the holes.
 const scoreBoard = document.querySelector('.score'); //select the score.
 const moles = document.querySelectorAll('.mole'); //select all the moles.
 const countDown = document.querySelector('.timer'); //select the score.
+const level = document.querySelector('.level'); //select the level.
 
 let lastHole; //this variable is needed in the randomHole function to avoid that the same hole is picked twice or more consecutively.
 let timeUp = false; //this variable is used for being able to control the time frame in which the mole pops up and down from the holes...it is used in the popMole function.
-let score;
+let score; //variable used in startGame and popMole functions to set the score of the game.
+let difficultyLevel; //variable used in selectLevel and popMole functions to check the selected level of difficulty of the game and change the time range for the random pop of the mole.
+let minTime; //variable used in selectLevel and popMole functions for the min time for the pop of the mole
+let maxTime; //variable used in selectLevel and popMole functions for the max time for the pop of the mole
 
 //generate random Time amount:
 function randomTime(min, max) {
@@ -15,7 +19,7 @@ function randomTime(min, max) {
 
 //Pick the hole randomly:
 function randomHole(holes) {
-  const idx = Math.floor(Math.random() * holes.length);//generate a number between 1 and themax number of holes.
+  const idx = Math.floor(Math.random() * holes.length); //generate a number between 1 and themax number of holes.
   const hole = holes[idx]; //assign to "hole" a random hole in our holes list.
   // next we make sure that the same number for the hole doesn't appear consecutively.
   if(hole === lastHole) {
@@ -24,14 +28,25 @@ function randomHole(holes) {
   lastHole = hole; //set the lastHole equal to the last picked hole.
   return hole;
 }
+//Select level of difficulty:
+function selectLevel() {
+  difficultyLevel = level.value; //get the value of the selected level;
+  if (difficultyLevel !=1) { //set values for the difficult level.
+    minTime = 400;
+    maxTime = 800;
+  } else { //set values for the easy level.
+    minTime = 500;
+    maxTime = 1000;
+  }
+}
 
 //Pop the mole from the hole:
 function popMole() {
-  //getting the random time for the pop
-  const time = randomTime(500, 900); //reduce the numbers (time) to make the game more difficult.
+  //getting the random time for the pop depending on the difficulty level selected:
+    const time = randomTime(minTime, maxTime);
+   //reduce the numbers (time) to make the game more difficult.
   //getting the random holes where to pop
   const hole = randomHole(holes);
-
   //add the "pop" class to our div containing the class of hole in order to generate the pop effect for the mole:
   hole.classList.add('pop');
   //remove the mole after a random amount of time by creating a setTimeout function:
