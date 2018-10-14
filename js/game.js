3aaab4c3b531235b85e2d8ff35eb38c8d149e3da
Fabrizio Marras <fmarras79@gmail.com>
2018-10-14 18:22:17 +0200

    updated function that select the holes randomly to make sure it doesn't pick the same hole twice or more times consecutively.

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const holes = document.querySelectorAll('.hole'); //select all the holes.
 const scoreBoard = document.querySelector('.score'); //select the score.
 const moles = document.querySelectorAll('.mole'); //select all the moles.
 
+let lastHole; //this variable is needed in the randomHole function to avoid that the same hole is picked twice or more consecutively.
+
 //generate random Time amount:
 function randomTime(min, max) {
  return Math.round(Math.random() * (max - min) + min);
@@ -10,8 +12,12 @@ function randomTime(min, max) {
 
 //Pick the hole randomly:
 function randomHole(holes) {
-  const idx = Math.floor(Math.random() * holes.length); //generate a number between 1 and the max number of holes.
-  const hole = holes[idx]; //assign the holes[number] in our holes list to the "hole".
-
+  const idx = Math.floor(Math.random() * holes.length);//generate a number between 1 and themax number of holes.
+  const hole = holes[idx]; //assign to "hole" a random hole in our holes list.
+  // next we make sure that the same number for the hole doesn't appear consecutively.
+  if(hole === lastHole) {
+    return randomHole(holes); // run the function again to pick a new hole.
+  }
+  lastHole = hole; //set the lastHole equal to the last picked hole.
   return hole;
 }
