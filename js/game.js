f061fa99a4a5c9358eee083c968eb19cf2cd840c
Fabrizio Marras <fmarras79@gmail.com>
2018-10-14 18:03:44 +0200

    created a javascript file: select elements from the DOM

---
+++ js/game.js
@@ -0,0 +1,4 @@
+// Getting HTML document elements.
+const holes = document.querySelectorAll('.hole'); //select all the holes.
+const scoreBoard = document.querySelector('.score'); //select the score.
+const moles = document.querySelectorAll('.mole'); //select all the moles.
