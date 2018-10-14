838f2f1790572f772146953d22db2478c7fde2f8
Fabrizio Marras <fmarras79@gmail.com>
2018-10-14 18:05:36 +0200

    function to generate a random amount time

--- js/game.js
+++ js/game.js
@@ -2,3 +2,8 @@
 const holes = document.querySelectorAll('.hole'); //select all the holes.
 const scoreBoard = document.querySelector('.score'); //select the score.
 const moles = document.querySelectorAll('.mole'); //select all the moles.
+
+//generate random Time amount:
+function randomTime(min, max) {
+ return Math.round(Math.random() * (max - min) + min);
+}
